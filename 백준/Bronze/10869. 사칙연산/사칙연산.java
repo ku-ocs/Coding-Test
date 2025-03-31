922b92f3318a1f90fd06af316f31114f5b745bb2
SalKu <168716549+ku-ocs@users.noreply.github.com>
2025-03-31 10:19:50 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
+        
+        int num1 = Integer.parseInt(stz.nextToken());
+        int num2 = Integer.parseInt(stz.nextToken());
+        
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+    }
+}
