6351a774b11e38ee456708ea2e6c58caf1ee0ebc
SalKu <168716549+ku-ocs@users.noreply.github.com>
2025-04-22 09:49:44 +0900

    [Gold V] Title: 치킨 배달, Time: 152 ms, Memory: 15068 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m, dist, answer, hIdx, cIdx;
+    static int[][] town, home, chicken, selChicken;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(stz.nextToken());
+        m = Integer.parseInt(stz.nextToken());
+        town = new int[n][n];
+        home = new int[2*n][2];
+        chicken = new int[13][2];
+        selChicken = new int[m][2];
+
+        for(int i = 0; i < n; i++) {
+            stz = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                int building = Integer.parseInt(stz.nextToken());
+                town[i][j] = building;
+
+                if (building == 1) home[hIdx++] = new int[] {i ,j};
+                if (building == 2) chicken[cIdx++] = new int[] {i, j};
+            }
+        }
+
+        bt(0, 0);
+
+        System.out.println(answer);
+    }
+
+    public static void bt(int idx, int depth) {
+        if(depth == m) {
+            dist = 0;
+            calcDist();
+            return;
+        }
+
+        for (int i = idx; i < cIdx; i++) {
+            selChicken[depth] = chicken[i];
+            bt(i+1, depth+1);
+        }
+    }
+
+    public static void calcDist() {
+        for (int i = 0; i < hIdx; i++) {
+            int[] hArr = home[i];
+            int tempDist = 0;
+            for (int[] cArr : selChicken) {
+                int dx = Math.abs(hArr[0] - cArr[0]);
+                int dy = Math.abs(hArr[1] - cArr[1]);
+
+                int d = dx + dy;
+
+                if (tempDist == 0) tempDist = d;
+                else tempDist = Math.min(tempDist, d);
+            }
+            dist += tempDist;
+        }
+        if (answer == 0) answer = dist;
+        else answer = Math.min(answer, dist);
+    }
+}
+
