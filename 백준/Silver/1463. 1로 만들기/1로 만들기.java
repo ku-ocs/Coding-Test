78b4a7e572205cff4dabe5fd808db40dfa315138
SalKu <168716549+ku-ocs@users.noreply.github.com>
2025-04-22 16:26:52 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18104 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -9,19 +9,12 @@ public class Main {
         D[1] = 0;
 
         for(int i = 2; i <= num; i++) {
-            int val;
-            if (i % 3 == 0) {
-                val = D[i/3] + 1;
-                D[i] = D[i] == 0 ? val : Math.min(D[i], val);
-            }
-            if (i % 2 == 0) {
-                val = D[i/2] + 1;
-                D[i] = D[i] == 0 ? val : Math.min(D[i], val);
-            }
-            val = D[i-1] + 1;
-            D[i] = D[i] == 0 ? val : Math.min(D[i], val);
+            int val = D[i-1] + 1;
+            D[i] = val;
+            if (i % 3 == 0) D[i] = Math.min(D[i], D[i/3] + 1);
+            if (i % 2 == 0) D[i] = Math.min(D[i], D[i/2] + 1);
         }
-        
+
         System.out.println(D[num]);
     }
 }
