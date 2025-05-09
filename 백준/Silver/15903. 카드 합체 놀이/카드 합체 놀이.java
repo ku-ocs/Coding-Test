7b3379f9e002d3403fc29611636f832c6fe1f14c
SalKu <168716549+ku-ocs@users.noreply.github.com>
2025-05-09 19:01:20 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 152 ms, Memory: 15252 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.java
@@ -4,32 +4,30 @@ import java.util.*;
 public class Main {
     static int N, M;
     static long answer;
-    static long[] arr;
+    static PriorityQueue<Long> queue = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(stz.nextToken());
         M = Integer.parseInt(stz.nextToken());
-        arr = new long[N];
 
 
         stz = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(stz.nextToken());
+            queue.offer(Long.parseLong(stz.nextToken()));
         }
 
         for (int i = 0; i < M; i++) {
-            Arrays.sort(arr);
-            long num1 = arr[0];
-            long num2 = arr[1];
+            long num1 = queue.poll();
+            long num2 = queue.poll();
 
-            arr[0] = num1+num2;
-            arr[1] = num1+num2;
+            queue.offer(num1+num2);
+            queue.offer(num1+num2);
         }
 
-        for (long i : arr) {
-            answer += i;
+        while (!queue.isEmpty()) {
+            answer += queue.poll();
         }
 
         System.out.println(answer);
