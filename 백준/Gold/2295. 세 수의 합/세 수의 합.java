d8c04c56fb0796182f6e17dfdc42d68cf82eacd2
SalKu <168716549+ku-ocs@users.noreply.github.com>
2025-05-25 16:53:49 +0900

    [Gold IV] Title: 세 수의 합, Time: 272 ms, Memory: 47256 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -3,12 +3,14 @@ import java.util.*;
 
 public class Main {
     static int N;
-    static int[] arr, two;
-    static Set<Integer> set = new TreeSet<>();
+    static int[] arr;
+    static Set<Integer> twoSumSet = new HashSet<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
+
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
@@ -16,25 +18,15 @@ public class Main {
         Arrays.sort(arr);
 
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                set.add(arr[i]+arr[j]);
+            for (int j = i; j < N; j++) {
+                twoSumSet.add(arr[i] + arr[j]);
             }
         }
 
-        two = set.stream().mapToInt(Integer::intValue).toArray();
-
-        for (int i = N-1; i >= 0; i--) {
-            for (int j = N-1; j >= 0; j--) {
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = 0; j < N; j++) {
                 int target = arr[i] - arr[j];
-                int s = 0;
-                int e = two.length;
-                while (s < e) {
-                    int m = (s + e) / 2;
-                    if(two[m] >= target) e = m;
-                    else s = m+1;
-                }
-
-                if (two[s] == target) {
+                if (twoSumSet.contains(target)) {
                     System.out.println(arr[i]);
                     return;
                 }
