3842f8537a454c88abfbeac386f5af0d4bfb9bbb
SalKu <168716549+ku-ocs@users.noreply.github.com>
2025-05-25 09:53:10 +0900

    [Silver II] Title: 좌표 압축, Time: 3228 ms, Memory: 234800 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static Long[] arr, arr2;
+    static Set<Long> set = new HashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        arr = new Long[N];
+
+        StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            long l = Long.parseLong(stz.nextToken());
+            arr[i] = l;
+            set.add(l);
+        }
+
+        arr2 = set.toArray(new Long[0]);
+        Arrays.sort(arr2);
+
+        for (long l : arr) {
+            int s = 0;
+            int e = arr2.length;
+
+            while (s < e) {
+                int m = (s+e) / 2;
+                if (arr2[m] >= l) e = m;
+                else s = m+1;
+            }
+
+            bw.write(s+ " ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
