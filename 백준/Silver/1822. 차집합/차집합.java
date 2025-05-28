5632ad570c55cf792ee4dec44d92f4a994fc70b1
SalKu <168716549+ku-ocs@users.noreply.github.com>
2025-05-28 12:21:11 +0900

    [Silver IV] Title: 차집합, Time: 1692 ms, Memory: 158548 KB -BaekjoonHub

--- 백준/Silver/1822. 차집합/차집합.java
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -3,39 +3,61 @@ import java.util.*;
 
 public class Main {
     static int N, M;
-    static TreeSet<Integer> set1 = new TreeSet<>();
-    static HashSet<Integer> set2 = new HashSet<>();
+    static int[] A, B;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
 
-        N = Integer.parseInt(stz.nextToken());
-        M = Integer.parseInt(stz.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        stz = new StringTokenizer(br.readLine(), " ");
+        A = new int[N];
+        B = new int[M];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            set1.add(Integer.parseInt(stz.nextToken()));
+            A[i] = Integer.parseInt(st.nextToken());
         }
-
-        stz = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            set2.add(Integer.parseInt(stz.nextToken()));
+            B[i] = Integer.parseInt(st.nextToken());
         }
 
-        set1.removeAll(set2);
-        
-        if (set1.isEmpty()) {
-            System.out.println(0);
-        } else {
-            bw.write(set1.size() + "\n");
-            for (int i : set1) {
-                bw.write(i + " ");
+        Arrays.sort(B);
+
+        List<Integer> result = new ArrayList<>();
+        for (int i : A) {
+            if (!binarySearch(B, i)) {
+                result.add(i);
             }
-            bw.flush();
         }
 
+        if (result.isEmpty()) {
+            bw.write("0");
+        } else {
+            Collections.sort(result);
+            bw.write(result.size() + "\n");
+            for (int x : result) {
+                bw.write(x + " ");
+            }
+        }
+
+        bw.flush();
+        bw.close();
         br.close();
+    }
+
+    private static boolean binarySearch(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length;
+        while (s < e) {
+            int m = (s + e) / 2;
+            if (arr[m] == target) return true;
+            else if (arr[m] > target) e = m;
+            else s = m+1;
+        }
+        return false;
     }
 }
