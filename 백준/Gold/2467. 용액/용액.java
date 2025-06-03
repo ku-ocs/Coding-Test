61604c1783f1d519a49ad006e6b998912013b124
SalKu <168716549+ku-ocs@users.noreply.github.com>
2025-06-03 20:54:47 +0900

    [Gold V] Title: 용액, Time: 328 ms, Memory: 28636 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,79 +1,38 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, sum = Integer.MAX_VALUE;
     static int[] arr, answer = new int[2];
-    static boolean isMinus, isPlus;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
 
         StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(stz.nextToken());
-            arr[i] = num;
-            if (!isMinus && num < 0) {
-                isMinus = true;
-            }
-            if (!isPlus && num > 0) {
-                isPlus = true;
-            }
+            arr[i] = Integer.parseInt(stz.nextToken());
         }
-
-        if (isMinus && !isPlus) {
-            System.out.println(arr[N-2] + " " + arr[N-1]);
-            return;
-        }
-
-        if (isPlus && !isMinus) {
-            System.out.println(arr[0] + " " + arr[1]);
-            return;
-        }
-
-        for (int i : arr) {
-            int s = 0;
-            int e = arr.length-1;
-            if (sum == 0) {
-                break;
+        int s = 0;
+        int e = N-1;
+        while (s < e) {
+            int tempSum = arr[s] + arr[e];
+            if (sum > Math.abs(tempSum)) {
+                answer[0] = arr[s];
+                answer[1] = arr[e];
+                if (tempSum == 0) break;
+                sum = Math.abs(tempSum);
             }
 
-            while (s <= e) {
-                int m = (s + e) / 2;
-                int tempSum = i + arr[m];
-                if (sum > Math.abs(tempSum)) {
-                    if (i != arr[m]) {
-                        answer[0] = i;
-                        answer[1] = arr[m];
-                        sum = Math.abs(tempSum);
-                    }
-                    if (tempSum > 0) {
-                        e = m - 1;
-                    } else if (tempSum < 0) {
-                        s = m + 1;
-                    } else {
-                        break;
-                    }
-                } else {
-                    if (tempSum > 0) {
-                        e = m - 1;
-                    } else{
-                        s = m + 1;
-                    }
-                }
+            if (tempSum > 0) {
+                e--;
+            } else {
+                s++;
             }
         }
 
-        Arrays.sort(answer);
-
-        bw.write(answer[0] + " " + answer[1]);
-
-        bw.flush();
-        bw.close();
+        System.out.println(answer[0] + " " + answer[1]);
         br.close();
     }
 }
