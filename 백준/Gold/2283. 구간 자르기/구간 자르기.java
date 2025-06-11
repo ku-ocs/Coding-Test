720c3e4bcb7e94fea61235e09b15eee25a2c489b
SalKu <168716549+ku-ocs@users.noreply.github.com>
2025-06-11 16:37:46 +0900

    [Gold II] Title: 구간 자르기, Time: 200 ms, Memory: 26104 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.java
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.java
@@ -3,60 +3,50 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, K, length, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
-    static int[][] arr;
-    static int[] answerArr = new int[2];
+    static int[] len = new int[1000001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(stz.nextToken());
         K = Integer.parseInt(stz.nextToken());
-        arr = new int[N][2];
 
         for (int i = 0; i < N; i++) {
             stz = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(stz.nextToken());
-            int end = Integer.parseInt(stz.nextToken());
-            min = Math.min(start, min);
-            max = Math.max(end, max);
-            arr[i] = new int [] {start, end};
+            int s = Integer.parseInt(stz.nextToken());
+            int e = Integer.parseInt(stz.nextToken());
+            max = Math.max(max, e);
+            min = Math.min(min, s);
+            len[s]++;
+            len[e]--;
+        }
+
+        for (int i = min+1; i <= max; i++) {
+            len[i] += len[i-1];
+        }
+
+        int[] prefixSum = new int[max+1];
+        for (int i = min+1; i <= max; i++) {
+            prefixSum[i] = prefixSum[i-1] + len[i-1];
         }
 
         int s = 0;
         int e = 0;
-        for (int i = 0; i < N; i++) {
-            int lineS = arr[i][0];
-            if (lineS >= e) {
-                continue;
-            }
-            length++;
-        }
-
-        while (s <= e && e <= max) {
+        int answerS = 0;
+        int answerE = 0;
+        while (e <= max) {
+            length = prefixSum[e] - prefixSum[s];
             if (length == K) {
-                answerArr = new int[] {s, e};
+                answerS = s;
+                answerE = e;
                 break;
-            } else if (length < K) {
+            } else if(length < K) {
                 e++;
-                for (int i = 0; i < N; i++) {
-                    int lineS = arr[i][0];
-                    int lineE = arr[i][1];
-                    if (e > lineS && e <= lineE) {
-                        length++;
-                    }
-                }
             } else {
                 s++;
-                for (int i = 0; i < N; i++) {
-                    int lineS = arr[i][0];
-                    int lineE = arr[i][1];
-                    if (s > lineS && s <= lineE) {
-                        length--;
-                    }
-                }
             }
         }
 
-        System.out.println(answerArr[0] + " " + answerArr[1]);
+        System.out.printf("%d %d", answerS, answerE);
     }
 }
