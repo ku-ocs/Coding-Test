f70becc85f4834710d008d3af05072826a9b3f96
Oh Chang Sik <ku.ocs.94@gmail.com>
2025-07-08 14:24:04 +0900

    [Silver II] Title: 최소 힙, Time: 288 ms, Memory: 26632 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int order = Integer.parseInt(br.readLine());
+
+            if (order == 0) {
+                if (pq.isEmpty()) {
+                    bw.write(0 + "\n");
+                } else {
+                    bw.write(pq.poll() + "\n");
+                }
+            } else {
+                pq.offer(order);
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
