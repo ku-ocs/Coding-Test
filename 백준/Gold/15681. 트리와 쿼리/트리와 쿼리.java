7feaeaa37b3dc8eb1419fb004eabbb51c0443310
Oh Chang Sik <ku.ocs.94@gmail.com>
2025-07-16 14:48:34 +0900

    [Gold V] Title: 트리와 쿼리, Time: 848 ms, Memory: 79792 KB -BaekjoonHub

--- 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
+++ 백준/Gold/15681. 트리와 쿼리/트리와 쿼리.java
@@ -4,8 +4,7 @@ import java.io.*;
 public class Main {
     static int N, R, Q;
     static int[] dp;
-    static boolean[] vis;
-    static ArrayList<Integer>[] lineList;
+    static List<Integer>[] lineList;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -16,7 +15,6 @@ public class Main {
         Q = Integer.parseInt(stz.nextToken());
 
         dp = new int[N+1];
-        vis = new boolean[N+1];
         lineList = new ArrayList[N+1];
 
         for (int i = 0; i <= N; i++) {
@@ -32,9 +30,7 @@ public class Main {
             lineList[v].add(u);
         }
 
-        Arrays.fill(dp, 1);
-        cntNode(R);
-        dp[R] = N;
+        cntNode(R, 0);
 
         for (int i = 0; i < Q; i++) {
             bw.write(dp[Integer.parseInt(br.readLine())] + "\n");
@@ -45,18 +41,12 @@ public class Main {
         br.close();
     }
 
-    public static void cntNode(int n) {
-        ArrayList<Integer> list = lineList[n];
-        vis[n] = true;
+    public static void cntNode(int n, int p) {
+        dp[n] = 1;
 
-        if (n != R && list.size() == 1) {
-            dp[n] = 1;
-            return;
-        }
-
-        for (int l : list) {
-            if (vis[l]) continue;
-            cntNode(l);
+        for (int l : lineList[n]) {
+            if (l == p) continue;
+            cntNode(l, n);
             dp[n] += dp[l];
         }
     }
