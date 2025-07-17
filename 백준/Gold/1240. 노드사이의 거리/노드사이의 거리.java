a574d71a1d0be68d66b46de7f1d4c884c8a1e92b
Oh Chang Sik <ku.ocs.94@gmail.com>
2025-07-17 17:44:49 +0900

    [Gold V] Title: 노드사이의 거리, Time: 216 ms, Memory: 20552 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -2,10 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M;
-    static int[] length;
+    static int N, M, minLength;
     static ArrayList<int[]>[] lineList;
-    static Queue<int[]> queue = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -15,7 +13,6 @@ public class Main {
         M = Integer.parseInt(stz.nextToken());
 
         lineList = new ArrayList[N+1];
-        length = new int[N+1];
 
         for (int i = 1; i <= N; i++) {
             lineList[i] = new ArrayList<>();
@@ -34,7 +31,9 @@ public class Main {
             stz = new StringTokenizer(br.readLine(), " ");
             int s = Integer.parseInt(stz.nextToken());
             int e = Integer.parseInt(stz.nextToken());
-            bw.write(getLength(s, e) + "\n");
+            minLength = Integer.MAX_VALUE;
+            getLength(s, 0, e, 0);
+            bw.write(minLength + "\n");
         }
 
         bw.flush();
@@ -42,29 +41,16 @@ public class Main {
         br.close();
     }
 
-    public static int getLength(int start, int end) {
-        init();
-        queue.offer(new int[] {start, 0});
-        while (!queue.isEmpty()) {
-            int[] arr = queue.poll();
-            int n1 = arr[0];
-            int l1 = arr[1];
-            for (int[] lArr : lineList[n1]) {
-                int n2 = lArr[0];
-                int l2 = lArr[1];
-                int tL = l1 + l2;
-                if (n2 == start) continue;
-                if (length[n2] != 0 && length[n2] <= tL) continue;
-                length[n2] = tL;
-                queue.offer(new int[] {n2, tL});
+    public static void getLength(int start, int p, int end, int length) {
+        for(int[] arr : lineList[start]) {
+            int e = arr[0];
+            int l = arr[1];
+            if (e == p) continue;
+            if (e == end) {
+                minLength = Math.min(minLength, length + l);
+            } else {
+                getLength(e, start, end, length+l);
             }
         }
-
-        return length[end];
-    }
-
-    public static void init() {
-        Arrays.fill(length, 0);
-        queue.clear();
     }
 }
