0596dd1c59601c38409a3424b121226f53066746
Oh Chang Sik <ku.ocs.94@gmail.com>
2025-07-29 16:02:43 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 456 ms, Memory: 48072 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int V, E, answer;
+	static int[] p;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stz = new StringTokenizer(br.readLine(), " ");
+		
+		V = Integer.parseInt(stz.nextToken());
+		E = Integer.parseInt(stz.nextToken());
+
+		p = new int[V+1];
+		Arrays.fill(p, -1);
+		
+		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[0] - o2[0];
+			}
+		});
+		
+		for (int i = 0; i < E; i++) {
+			stz = new StringTokenizer(br.readLine(), " ");
+			int s = Integer.parseInt(stz.nextToken());
+			int e = Integer.parseInt(stz.nextToken());
+			int v = Integer.parseInt(stz.nextToken());
+			pq.offer(new int[] {v, s, e});
+		}
+
+		while(!pq.isEmpty()) {
+			int[] line = pq.poll();
+			int v = line[0];
+			int s = line[1];
+			int e = line[2];
+			if (union(s, e)) {
+				answer += v;
+			}
+		}
+
+		System.out.println(answer);
+	}
+
+	public static int find(int n) {
+		if (p[n] < 0) return n;
+		else return p[n] = find(p[n]);
+	}
+
+	public static boolean union(int n1, int n2) {
+		n1 = find(n1);
+		n2 = find(n2);
+		if (n1 == n2) return false;
+		if (p[n2] < p[n1]) {
+			int temp = n2;
+			n2 = n1;
+			n1 = temp;
+		}
+		if (p[n2] == p[n1]) p[n1]--;
+		p[n2] = n1;
+		return true;
+	}
+}
