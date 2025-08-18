a95f4e7d87fb4a6d34f69337b535e8955c6162f3
Oh Chang Sik <ku.ocs.94@gmail.com>
2025-08-18 13:38:44 +0900

    [Gold IV] Title: 알고스팟, Time: 164 ms, Memory: 16628 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -2,13 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int N, M;
+	static int N, M, INF = 1_000_000_000;
 	static int[][] map;
 	static int[][] dist;
-	static boolean[][] vis;
 	static int[] dx = new int[] {-1, 0, 1, 0};
 	static int[] dy = new int[] {0, 1, 0, -1};
-	static Queue<int[]> queue = new LinkedList<>();
+	static Deque<int[]> dq = new ArrayDeque<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] sArr = br.readLine().split(" ");
@@ -18,32 +17,31 @@ public class Main {
 
 		map = new int[N][M];
 		dist = new int[N][M];
-		vis = new boolean[N][M];
 
 		for (int i = 0; i < N; i++) {
 			sArr = br.readLine().split("");
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(sArr[j]);
+				dist[i][j] = INF;
 			}
 		}
 
-		queue.offer(new int[] {0, 0, 0});
-		vis[0][0] = true;
+		dq.offer(new int[] {0, 0});
+		dist[0][0] = 0;
 		
-		while(!queue.isEmpty()) {
-			int[] cur = queue.poll();
+		while(!dq.isEmpty()) {
+			int[] cur = dq.poll();
 			int curX = cur[0];
 			int curY = cur[1];
-			int curCnt = cur[2];
 			for (int i = 0; i < 4; i++) {
 				int nxtX = curX + dx[i];
 				int nxtY = curY + dy[i];
 				if (nxtX >= N || nxtX < 0 || nxtY >= M || nxtY < 0) continue;
-				int nxtCnt = map[nxtX][nxtY] == 1 ? curCnt+1 : curCnt;
-				if (nxtCnt >= dist[nxtX][nxtY] && vis[nxtX][nxtY]) continue;
-				vis[nxtX][nxtY] = true;
-				dist[nxtX][nxtY] = nxtCnt;
-				queue.offer(new int[] {nxtX, nxtY, nxtCnt});
+				int cnt = map[nxtX][nxtY];
+				if (dist[nxtX][nxtY] <= dist[curX][curY] + cnt) continue;
+				dist[nxtX][nxtY] = dist[curX][curY] + cnt;
+				if (cnt == 0) dq.addFirst(new int[] {nxtX, nxtY});
+				else dq.addLast(new int[] {nxtX, nxtY});
 			}
 		}
 
